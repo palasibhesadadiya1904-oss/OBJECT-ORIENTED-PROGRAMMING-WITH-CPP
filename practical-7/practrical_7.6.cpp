c5cb359423a7888d60bbefd0d98b46be70f118d4
palasibhesadadiya1904-oss <palasibhesadadiya1904@gmail.com>
2026-04-20 19:27:00 +0530

    Add files via upload

---
+++ practical-7/practrical_7.6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base Constructor" << endl;
+    }
+
+    // Virtual destructor
+    virtual ~Base() {
+        cout << "Base Destructor" << endl;
+    }
+};
+
+class Derived : public Base {
+private:
+    int* ptr;
+
+public:
+    Derived() {
+        cout << "Derived Constructor" << endl;
+        ptr = new int[3];
+    }
+
+    ~Derived() {
+        cout << "Derived Destructor" << endl;
+        delete[] ptr;
+    }
+};
+
+int main() {
+    Base* b;
+    b = new Derived();
+
+    cout << "\nDeleting object...\n";
+    delete b;
+    cout<<"bhesdadiya palasi _25ce008" ;
+
+    return 0;
+}
+
